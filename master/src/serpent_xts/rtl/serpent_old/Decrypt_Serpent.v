a0aaaa72383fb9dc465bd5678523866095277cc5
sugarete <cthien3602@gmail.com>
2024-06-05 08:50:23 +0700

    Update .gitignore

--- master/src/serpent_xts/rtl/serpent_old/Decrypt_Serpent.v
+++
@@ -1,7 +0,0 @@
-module Decrypt_Serpent (
-    input wire [255:0] i_key,
-    input wire [127:0] i_data,
-    output wire [127:0] o_data
-);
-    
-endmodule
--- master/src/serpent_xts/rtl/serpent_old/Encrypt_Serpent.v
+++
@@ -1,8 +0,0 @@
-module Encrypt_Serpent (
-    input wire i_key_valid,
-    input wire [255:0] i_key,
-    input wire [127:0] i_data,
-    output wire [127:0] o_data
-);
-    
-endmodule
